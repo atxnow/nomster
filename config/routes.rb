33b54f51e0b651f8113fcceb0bc680582e587c98
Thomas Petrini <thomaslpetrini@gmail.com>
2015-06-02 02:45:28 +0000

    added user dashboardbeginnings

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Nomster::Application.routes.draw do
-
-devise_for :users
-root 'places#index'
-
-   resources :places do
+  devise_for :users
+  root 'places#index'
+  resources :places do
     resources :comments, :only => :create
+  end
+  resources :users, :only => :show
 
-    resource :photos, :only => :create
 
- end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
